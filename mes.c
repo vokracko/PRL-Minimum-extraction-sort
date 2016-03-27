378a379fc5d69e1bcdf33695d22e5455f90b0c17
Lukáš Vokráčko <lukas@vokracko.cz>
2016-03-27 16:48:57 +0200

    not sorting

---
+++ mes.c
@@ -0,0 +1,119 @@
+#include <mpi.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define TAG 0
+#define EMPTY 256
+#define SEND(cx, id) MPI_Send(&cx, 1, MPI_INT, id, TAG, MPI_COMM_WORLD)
+#define RECV(cx, id) MPI_Recv(&cx, 1, MPI_INT, id, TAG, MPI_COMM_WORLD, &stat)
+
+
+bool leaf(int i, int n)
+{
+	return i >= (n-1);
+}
+
+
+int main(int argc, char * argv[])
+{
+	MPI_Status stat;
+	int number;
+	int cpu_id;
+	int p_count;
+	int n;
+
+	MPI_Init(&argc,&argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &p_count);
+	MPI_Comm_rank(MPI_COMM_WORLD, &cpu_id);
+
+
+	n = (p_count+1)/2;
+
+	if(cpu_id == 0)
+	{
+		int recv_id = p_count-1;
+		FILE * handle = fopen("numbers", "r");
+
+		while((number = fgetc(handle)) != EOF)
+		{
+			SEND(number, recv_id);
+
+			if(recv_id == p_count-1)
+				printf("%u", number);
+			else
+				printf(" %u", number);
+
+			recv_id--;
+		}
+
+		printf("\n");
+
+		number = EMPTY;
+
+		while(recv_id >= 0)
+		{
+			SEND(number, recv_id);
+			recv_id--;
+		}
+
+		fclose(handle);
+	}
+
+
+	if(leaf(cpu_id, n))
+		RECV(number, 0);
+	else
+		number = EMPTY;
+
+	for(int i = 0; i < 2*n+log2(n)-1; ++i)
+	{
+		int left;
+		int right;
+
+		if(leaf(cpu_id, n))
+		{
+			SEND(number, (cpu_id - 1) / 2);
+			RECV(number, (cpu_id - 1) / 2);
+		}
+		else
+		{
+			RECV(left, cpu_id * 2 + 1);
+			RECV(right, cpu_id * 2 + 2);
+			
+			if(number == EMPTY)
+			{
+				if(left < right)
+				{
+					number = left;
+					left = EMPTY;
+				}
+				else 
+				{
+					number = right;
+					right = EMPTY;
+				}
+			}
+
+			SEND(left, cpu_id * 2 + 1);
+			SEND(right, cpu_id * 2 + 2);
+
+			if(cpu_id != 0)
+			{
+				SEND(number, (cpu_id - 1) / 2);
+				RECV(number, (cpu_id - 1) / 2);
+			}
+
+			if(cpu_id == 0 && number != EMPTY)
+			{
+				printf("%d\n", number);
+				number = EMPTY;
+			}
+		}
+	}
+
+	MPI_Finalize();
+
+	return 0;
+}
